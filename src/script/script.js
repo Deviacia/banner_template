e995ede6727a287c4e2faacf1f7ee365e9243f3f
Deviacia <ivan.bpemr@gmail.com>
2021-12-01 14:59:00 +0200

    Google_spec

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,18 @@
-const loop = 1;
-const timeline = gsap.timeline({ repeat: loop, delay: 0 })
+// Animation
+    var loops = 0;
+    var tl = new TimelineMax({ repeat: -1, delay: 0 })
 
-// const onComplete=(loop)=>{
-//     if (loop >= 2) {
-//         timeline.pause();
-//         console.log(loop)
-//     } else {
-//         for (let i = 0; i <= loop; i++) {
-//             console.log(i)
-//             break;
-//         }
-//     }
-// }
+    // 1 key
+        .from('.people', 0.75, { opacity: '1', onComplete: function () { if (loops >= 2) { tl.pause() } else { loops++ }} })
+        .from('.t1', 0.75, { x: -900 }, '-=0.75')
+        .from('.cta', 0.75, { opacity: "0" }, '-=1.00')
+        .to('.btn', 0.25,{ opacity: '0' }, '+=4.00')
+        .to('.people', 0.25, { opacity: '0' }, '-=0.5')
+        .to('.t1', 0.25, { opacity: '0' }, '-=0.5')
+        .to('.cta', 0.25, { opacity: "0" }, '-=0.5')
+    // 2 key
 
-timeline.from('.button', {duration: 2.0, x: -400});
+        .from('.cards', 0.75, { x: 500 })
+        .from('.t2', 0.75, { x: 1000 })
+        .to('.t2', 0.75, { x: -1000, opacity: '0' }, '+=3.00')
+        .to('.cards', 0.55, { x: 500, ease: "slow(0.7, 0.7, false)"})
